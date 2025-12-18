c98890d4821a8d82a6cca711bd995dad02534eca
AC <drsnake1995@gmail.com>
2025-12-18 17:21:55 -0300

    se instancia la clase

--- controllers/mecanico/controlador_registrar_mecanico.php
+++ controllers/mecanico/controlador_registrar_mecanico.php
@@ -1,10 +1,26 @@
 <?php
     require '../../models/modelo_mecanico.php';
     
-    $MC= new modelo_Mecanico();
+    $MM= new modelo_Mecanico();
+    $usuario= htmlspecialchars($_POST['usuario'], ENT_QUOTES, 'UTF-8');
+    $contra= htmlspecialchars($_POST['contra'], ENT_QUOTES, 'UTF-8');
+    $estado= htmlspecialchars($_POST['estado'], ENT_QUOTES, 'UTF-8');
+    $nombre= htmlspecialchars($_POST['nombre'], ENT_QUOTES, 'UTF-8');
+    $apellido= htmlspecialchars($_POST['apellido'], ENT_QUOTES, 'UTF-8');
+    $rut= htmlspecialchars($_POST['rut'], ENT_QUOTES, 'UTF-8');
+    $email= htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+    $telefono= htmlspecialchars($_POST['telefono'], ENT_QUOTES, 'UTF-8');
+    $sexo= htmlspecialchars($_POST['sexo'], ENT_QUOTES, 'UTF-8');
+    $rol= htmlspecialchars($_POST['rol'], ENT_QUOTES, 'UTF-8');
+    $especialidad= htmlspecialchars($_POST['especialidad'], ENT_QUOTES, 'UTF-8');
+    $taller= htmlspecialchars($_POST['taller'], ENT_QUOTES, 'UTF-8');
     
-    //datos tabla usuario
+    $consulta = $MM->registrar_mecanico($usuario, $contra, $estado, $nombre, $apellido, $rut, $email, $telefono, $sexo, $rol, $especialidad, $taller);
+    echo $consulta;
 
+
+    //datos tabla usuario
+/*
     $usuario = htmlspecialchars($_POST['usuario'] ?? '', ENT_QUOTES, 'UTF-8');
     $contra = htmlspecialchars($_POST['contra'] ?? '', ENT_QUOTES, 'UTF-8');
     $sexo = htmlspecialchars($_POST['sexo'] ?? '', ENT_QUOTES, 'UTF-8');
@@ -26,5 +42,5 @@
 
     $consulta = $MC->registrar_mecanico_completo($usuario, $contra, $sexo, $rol, $rut, $nombre, $especialidad, $telefono, $email, $taller);
     echo $consulta;
-
+*/
 ?>
